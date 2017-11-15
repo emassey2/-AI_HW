5e5682aba7a127d97ba1fed4f7546fc8bc953a2f
Ethan Oswald Massey <ethan.massey@smail.inf.h-brs.de>
2017-11-15 20:36:28 +0100

    first almost kinda full implementation. No backtracking

--- AI_HW5_CODE/8-puzzle.py
+++ AI_HW5_CODE/8-puzzle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import copy
+import sys
 
 # I really wish python had enums...
 class Direction(object):
@@ -44,6 +45,10 @@ class eight_puzzle:
             state_str += '\n'
 
         return state_str
+
+
+    def set_cur_state(self, state):
+        self.cur_state = copy.deepcopy(state)
 
 
     # find each number and also it's index
@@ -105,43 +110,15 @@ class eight_puzzle:
 
 
     # returns all possible future states (doing only one move)
-    def get_possible_moves(self):
-        possible_moves = list()
+    def get_one_move_states(self):
+        possible_states = list()
         blank_row, blank_col = self.get_blank_position()
 
         for move in Direction.DIRECTIONS:
             if self.is_valid_move((blank_row, blank_col), move):
-                possible_moves.append(self.get_state_post_move(move))
+                possible_states.append(self.get_state_post_move(move))
 
-        return possible_moves
-
-    def get_manhattan_dist(self):
-        cur_positions = self.get_number_positions(self.cur_state)
-        goal_positions = self.get_number_positions(self.goal_state)
-
-        distance = 0
-
-        # sum the distance between our two states
-        for i in xrange(0, self.PUZZLE_SIDE_LENGTH**2):
-            distance += abs(cur_positions[i][1] - goal_positions[i][1]) + \
-                        abs(cur_positions[i][2] - goal_positions[i][2])
-
-        return distance
-
-
-    def get_num_misplaced_tiles(self):
-        cur_positions = self.get_number_positions(self.cur_state)
-        goal_positions = self.get_number_positions(self.goal_state)
-
-        misplaced_tiles = 0
-
-        # add one for each misplaced tile (compare x,y cords)
-        for i in xrange(0, self.PUZZLE_SIDE_LENGTH**2):
-            if (cur_positions[i][1] != goal_positions[i][1] or \
-                cur_positions[i][2] != goal_positions[i][2]):
-                misplaced_tiles += 1
-
-        return misplaced_tiles
+        return possible_states
 
 
     def get_state(self):
@@ -151,13 +128,96 @@ class eight_puzzle:
     def print_state(self):
         print self.get_state()
 
+
+
+def get_manhattan_dist(cur_puzzle, cur_state, goal_state):
+    cur_positions = cur_puzzle.get_number_positions(cur_state)
+    goal_positions = cur_puzzle.get_number_positions(goal_state)
+
+    distance = 0
+
+    # sum the distance between our two states
+    for i in xrange(0, puzzle.PUZZLE_SIDE_LENGTH**2):
+        distance += abs(cur_positions[i][1] - goal_positions[i][1]) \
+                  + abs(cur_positions[i][2] - goal_positions[i][2])
+
+    return distance
+
+
+def get_num_misplaced_tiles(cur_puzzle, cur_state, goal_state):
+    cur_positions = cur_puzzle.get_number_positions(cur_state)
+    goal_positions = cur_puzzle.get_number_positions(goal_state)
+
+    misplaced_tiles = 0
+
+    # add one for each misplaced tile (compare x,y cords)
+    for i in xrange(0, cur_puzzle.PUZZLE_SIDE_LENGTH**2):
+        if (cur_positions[i][1] != goal_positions[i][1] \
+            or cur_positions[i][2] != goal_positions[i][2]):
+            misplaced_tiles += 1
+
+    return misplaced_tiles
+
+
+def greedy_search(puzzle, heuristic, previous_states):
+    goal_achieved = False
+
+    # check for our goal state
+    score = heuristic(puzzle, puzzle.cur_state, puzzle.goal_state)
+    if score == 0:
+        goal_achieved = True
+
+    #print "cur_score", score, "\nState\n", puzzle.twoD_array_to_str(puzzle.cur_state)
+
+
+    # if we haven't found our goal, explore our state spaces
+    if not goal_achieved:
+        # first we get all of our possible moves
+        possible_states = puzzle.get_one_move_states()
+
+        # evaluate all of the moves
+        ranked_states = list()
+        for state in possible_states:
+            heuristic_score = heuristic(puzzle, state, puzzle.goal_state)
+            ranked_states.append((heuristic_score, state))
+
+        # sort and choose the first (best) move
+        ranked_states.sort()
+        top_ranked_state = ranked_states[0]
+        #print top_ranked_state[0], top_ranked_state[1]
+
+        # because we are greedy do the best move
+        puzzle.set_cur_state(top_ranked_state[1])
+        previous_states.append(top_ranked_state)
+        goal_achieved = greedy_search(puzzle, heuristic, previous_states)
+
+    return goal_achieved
+
+
+
 if __name__ == '__main__':
-    puzzle = eight_puzzle("012345678", "876543120")
-    puzzle.print_state()
-    print puzzle.twoD_array_to_str(puzzle.goal_state)
-    print puzzle.get_manhattan_dist()
-    print puzzle.get_num_misplaced_tiles()
-    print '\n'
-    moves =  puzzle.get_possible_moves()
-    for move in moves:
-        print puzzle.twoD_array_to_str(move)
+    sys.setrecursionlimit(10000)
+    easy_puzzle = eight_puzzle("876514203", "876543210")
+    hard_puzzle = eight_puzzle("148362057", "876543210")
+    puzzles = [easy_puzzle, hard_puzzle]
+    for cur_puzzle in puzzles:
+        puzzle = copy.deepcopy(cur_puzzle)
+        print "Starting State"
+        puzzle.print_state()
+        print "Goal State"
+        print puzzle.twoD_array_to_str(puzzle.goal_state)
+        heuristics = [(get_num_misplaced_tiles, "misplaced tiles"), \
+                      (get_manhattan_dist, "manhattand distance")]
+        for heuristic in heuristics:
+            states = list()
+            print "\n\nTrying ", heuristic[1]
+            greedy_search(puzzle, heuristic[0], states)
+            print "Final State"
+            puzzle.print_state()
+
+            print "Path States"
+            for state in states:
+                print "Heuristic:", state[0], "\nState:\n", puzzle.twoD_array_to_str(state[1])
+            print "Press enter to continue"
+            raw_input()
+            puzzle = copy.deepcopy(cur_puzzle)
