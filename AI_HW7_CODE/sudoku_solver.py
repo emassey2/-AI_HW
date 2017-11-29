c84b06029b2e7339989cdb8cd68a8a88cfd9a958
Ethan Oswald Massey <ethan.massey@smail.inf.h-brs.de>
2017-11-29 20:19:11 +0100

    post merge and working on solving algo

--- AI_HW7_CODE/sudoku_solver.py
+++ AI_HW7_CODE/sudoku_solver.py
@@ -3,9 +3,10 @@
 
 from itertools import islice
 from sets import Set
+import time
 
 
-DEBUG_LEVEL = 1
+DEBUG_LEVEL = 2
 PRINT_STATEMENTS = True
 
 
@@ -50,46 +51,8 @@ class Sudoku:
 
         return state_str
 
-    def check_column(self, column_number):
-        column = list()
 
-        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
-            print "Checking col..."
-
-        # find every number in our column
-        for row in xrange(0, self.SUDOKU_SIDE_LENGTH):
-            column.append(self.state[row][column_number])
-
-        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
-            print column
-
-        # convert that column to a set for easy comparison
-        column_set = set(column)
-
-        # if our column doesn't contain all the numbers [1,9] return false
-        return len(column_set.difference(self.SUDOKU_SET)) == 0
-
-
-    def check_row(self, row_number):
-        row = list()
-
-        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
-            print "Checking row..."
-
-        # find every number in our row
-        for col in xrange(0, self.SUDOKU_SIDE_LENGTH):
-            row.append(self.state[row_number][col])
-
-        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
-            print row
-
-        # convert that row to a set for easy comparison
-        row_set = set(row)
-
-        # if our row doesn't contain all the numbers [1,9] return false
-        return len(row_set.difference(self.SUDOKU_SET)) == 0
-
-    def get_row(self,row_number):
+    def get_row_set(self,row_number):
         row = list()
         #Returns a set of all the current elements in a row
         if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
@@ -104,9 +67,11 @@ class Sudoku:
 
         # convert that row to a set for easy comparison
         row_set = set(row)
+
         return row_set
 
-    def get_colum(self,column_number):
+
+    def get_column_set(self,column_number):
         #Returns a set of all the current elements in a column
         column = list()
 
@@ -126,7 +91,8 @@ class Sudoku:
         # if our column doesn't contain all the numbers [1,9] return false
         return column_set
 
-    def get_sub_square(self,row,col):
+
+    def get_sub_square_set(self,row,col):
         #Returns a set of all the current alements in a sub square
         sub_square = list()
 
@@ -149,29 +115,83 @@ class Sudoku:
         # if our row doesn't contain all the numbers [1,9] return false
         return sub_square_set
 
-    def get_super_set(self,row,col):
-        #Initialize the output_set with all the possible numbers
-        output_set = self.SUDOKU_SET
+
+    def get_super_set(self, row, col):
         #Get the corresponding row_set
         row_set = self.get_row(row)
+
         #Get the corresponding col_set
         col_set = self.get_colum(col)
+
         #Converting the corresponding row and column to a subset of the sudoku
         sub_square_set = \
             self.get_sub_square(row/self.SUDOKU_SUB_SIZE,col/self.SUDOKU_SUB_SIZE)
-        #Create a super set of row, column and sub_square sets
+
+        #Create a super set of row, column, and sub_square sets
         super_set = row_set
         super_set.update(col_set)
         super_set.update(sub_square_set)
-        #Substract the numbers that are in the super_set to get the possible
-        #numbers
-        output_set.difference_update(super_set)
+
         if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
             print output_set
+
         return output_set
 
-    def check_sub_square(self, row, col):
-        sub_square = list()
+
+    # returns TRUE in the presence of blanks
+    def valid_column(self, column_number):
+        column = dict()
+        valid = True
+
+        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
+            print "Checking col..."
+
+        # use a dictionary to check for repeated numbers in our column
+        for row in xrange(0, self.SUDOKU_SIDE_LENGTH):
+            cur_value = self.state[row][column_number]
+
+            if cur_value != '0':
+                if cur_value in column:
+                    valid = False
+                    break;
+                else:
+                    column[cur_value] = cur_value
+
+        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
+            print column
+
+        return valid
+
+
+    # returns TRUE in the presence of blanks
+    def valid_row(self, row_number):
+        row = dict()
+        valid = True
+
+        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
+            print "Checking col..."
+
+        # use a dictionary to check for repeated numbers in our row
+        for col in xrange(0, self.SUDOKU_SIDE_LENGTH):
+            cur_value = self.state[row_number][col]
+
+            if cur_value != '0':
+                if cur_value in row:
+                    valid = False
+                    break;
+                else:
+                    row[cur_value] = cur_value
+
+        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
+            print row
+
+        return valid
+
+
+    # returns TRUE in the presence of blanks
+    def valid_sub_square(self, row, col):
+        sub_square = dict()
+        valid = True
 
         if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
             print "Checking sub square..."
@@ -181,21 +201,24 @@ class Sudoku:
         row_start = row*self.SUDOKU_SUB_SIZE
         for col in xrange(col_start, col_start + self.SUDOKU_SUB_SIZE):
             for row in xrange(row_start, row_start + self.SUDOKU_SUB_SIZE):
-                sub_square.append(self.state[row][col])
+                cur_value = self.state[row][col]
+
+            if cur_value != '0':
+                if cur_value in sub_square:
+                    valid = False
+                    break;
+                else:
+                    sub_square[cur_value] = cur_value
 
         if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
             print sub_square
 
-        # convert that row to a set for easy comparison
-        sub_square_set = set(sub_square)
-
-        # if our row doesn't contain all the numbers [1,9] return false
-        return len(sub_square_set.difference(self.SUDOKU_SET)) == 0
+        return valid
 
 
-    def check_sudoku(self):
+    # returns TRUE in the presence of blanks
+    def valid_sudoku(self):
         valid = True
-        self.get_super_set(0,4)
 
         # check all our sub squares in range [0, 2]
         for i in xrange(0, self.SUDOKU_SUB_SIZE):
@@ -218,6 +241,109 @@ class Sudoku:
 
         return valid
 
+
+    # returns FALSE in the presence of blanks
+    def solved_column(self, column_number):
+        column = list()
+
+        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
+            print "Checking col..."
+
+        # find every number in our column
+        for row in xrange(0, self.SUDOKU_SIDE_LENGTH):
+            column.append(self.state[row][column_number])
+
+        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
+            print column
+
+        # convert that column to a set for easy comparison
+        column_set = set(column)
+
+        # if our column doesn't contain all the numbers [1,9] return false
+        return len(column_set.difference(self.SUDOKU_SET)) == 0
+
+
+    # returns FALSE in the presence of blanks
+    def solved_row(self, row_number):
+        row = list()
+
+        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
+            print "Checking row..."
+
+        # find every number in our row
+        for col in xrange(0, self.SUDOKU_SIDE_LENGTH):
+            row.append(self.state[row_number][col])
+
+        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
+            print row
+
+        # convert that row to a set for easy comparison
+        row_set = set(row)
+
+        # if our row doesn't contain all the numbers [1,9] return false
+        return len(row_set.difference(self.SUDOKU_SET)) == 0
+
+
+    # returns FALSE in the presence of blanks
+    def solved_sub_square(self, row, col):
+        sub_square = list()
+
+        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
+            print "Checking sub square..."
+
+        # find every number sub square
+        col_start = col*self.SUDOKU_SUB_SIZE
+        row_start = row*self.SUDOKU_SUB_SIZE
+        for col in xrange(col_start, col_start + self.SUDOKU_SUB_SIZE):
+            for row in xrange(row_start, row_start + self.SUDOKU_SUB_SIZE):
+                sub_square.append(self.state[row][col])
+
+        if PRINT_STATEMENTS and DEBUG_LEVEL >= 3:
+            print sub_square
+
+        # convert that row to a set for easy comparison
+        sub_square_set = set(sub_square)
+
+        # if our row doesn't contain all the numbers [1,9] return false
+        return len(sub_square_set.difference(self.SUDOKU_SET)) == 0
+
+
+    # returns FALSE in the presence of blanks
+    def solved_sudoku(self):
+        valid = True
+
+        # check all our sub squares in range [0, 2]
+        for i in xrange(0, self.SUDOKU_SUB_SIZE):
+            for j in xrange(0, self.SUDOKU_SUB_SIZE):
+                valid = self.check_sub_square(i, j)
+
+                if not valid:
+                    break;
+
+            if not valid:
+                break;
+
+        # check all of our rows and columns
+        if valid:
+            for i in xrange(0, self.SUDOKU_SIDE_LENGTH):
+                valid = self.check_column(i) and self.check_row(i)
+
+                if not valid:
+                    break;
+
+        return valid
+
+
+    # search through our sudoku and find all the blank spaces (the 0s)
+    def find_zeros(self):
+        zero_locations = list()
+
+        for i in xrange(0, self.SUDOKU_SIDE_LENGTH):
+            for j in xrange(0, self.SUDOKU_SIDE_LENGTH):
+                if self.state[i][j] == '0':
+                    zero_locations.append((i,j))
+
+
     def set_cur_state(self, state):
         self.cur_state = copy.deepcopy(state)
 
@@ -239,6 +365,39 @@ class Sudoku:
         return delta
 
 
+    def get_all_successors(self):
+        return list(self.SUDOKU_SET)
+
+
+    def get_valid_successors(self):
+        #Initialize the output_set with all the possible numbers
+        output_set = self.SUDOKU_SET
+
+        super_set = self.get_super_set()
+
+        # find the difference between all possible numbers and our superset of
+        # currently present numbers
+        output_set.difference_update(super_set)
+
+        return list(output_set)
+
+
+
+def solve_sudoku(cur_state, successor_function):
+    # make sure this is even a vaild solution
+    if cur_state.valid_state():
+        return False
+
+    # if the puzzle is solved we are done
+    if cur_state.check_sudoku():
+        return True
+
+    # if it isn't solved yet, find the zeros and try different solutions
+    zero_locations = cur_state.find_zeros()
+
+    for zero_location in zero_locations:
+        pass #for
+
 
 def get_puzzles_from_file(puzzles_folder, puzzles_file):
     # defined by the file format
@@ -250,7 +409,7 @@ def get_puzzles_from_file(puzzles_folder, puzzles_file):
     with open(puzzles_folder + puzzles_file, "r") as puzzles:
 
         for puzzle in iter(lambda: tuple(islice(puzzles, LINES_PER_PUZZLE)), ()):
-            # trim the name and join the each puzzle into one long string
+            # trim the name and join each line of the puzzle into one long str
             puzzle_str = ''.join([str(puzzle_line).strip() for puzzle_line in puzzle[1:]])
 
             # make a new sudoku object from the string and add it to our list
@@ -259,6 +418,8 @@ def get_puzzles_from_file(puzzles_folder, puzzles_file):
             sudoku_puzzles.append(sudoku_puzzle)
 
     return sudoku_puzzles
+
+
 
 if __name__ == '__main__':
     puzzles_folder = './'
